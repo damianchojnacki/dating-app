7e7a436cdc619d3064a4fa10e159c93f035359f9
Damian Chojnacki <daamian3@gmail.com>
2020-04-16 20:37:08 +0200

    PreferencesCreator weightand height

--- src/pages/PreferencesCreator/PreferencesCreator.jsx
+++ src/pages/PreferencesCreator/PreferencesCreator.jsx
@@ -3,12 +3,20 @@ import classNames from 'classnames';
 import {FaCheckCircle} from "react-icons/fa";
 import Layout from "../../components/Layout";
 
+import "./preferencesCreator.scss";
+
 function PreferencesCreator() {
     const [step, setStep] = useState(3);
     const [hide, setHide] = useState(false);
     const [loading, setLoading] = useState(false);
     const [finished, setFinished] = useState(false);
     const [preferences, setPreferences] = useState({});
+
+    const minHeight = 1;
+    const maxHeight = 2;
+
+    const minWeight = 30;
+    const maxWeight = 120;
 
     const validate = (preferences) => {
         let check = false;
@@ -37,7 +45,7 @@ function PreferencesCreator() {
             case 2:
                 return ["man", "women"].includes(preferences.gender);
             case 3:
-                return false;
+                return preferences.height > minHeight && preferences.height < maxHeight;
             case 4:
                 return true;
             case 6:
@@ -102,10 +110,44 @@ function PreferencesCreator() {
         </div>,
         <div onKeyDown={handleKeyDown}>
             <h2 className="mb-8">
-                Wybierz swój wzrost oraz wagę:
+                Wzrost:
             </h2>
 
+            <div className="inline-block mb-4 bg-primary-700 text-white rounded p-2">{preferences.height ? Number(preferences.height).toFixed(2) + " m" : "?"}</div>
+            <input className="height outline-none focus:shadow-sm rounded-lg overflow-hidden appearance-none bg-gray-400 h-4 w-full" type="range" min={minHeight} max={maxHeight} step="0.01" defaultValue="1.75" onInput={e => setPreferences({...preferences, height: e.target.value})}/>
             <div className="flex justify-between">
+                <span>{minHeight.toFixed(2)} m</span>
+                <span>{(maxHeight * (5 / 8)).toFixed(2)} m</span>
+                <span>{(maxHeight * (6 / 8)).toFixed(2)} m</span>
+                <span>{(maxHeight * (7 / 8)).toFixed(2)} m</span>
+                <span>{maxHeight.toFixed(2)} m</span>
+            </div>
+
+            <div className="flex justify-between mt-8">
+                <button onClick={previousStep} type="button" className="w-1/3 py-4 bg-primary-700 text-white">
+                    Wróć
+                </button>
+                <button onClick={nextStep} type="button" className="w-1/3 py-4 bg-primary-700 text-white">
+                    Dalej
+                </button>
+            </div>
+        </div>,
+        <div onKeyDown={handleKeyDown}>
+            <h2 className="mb-8">
+                Waga:
+            </h2>
+
+            <div className="inline-block mb-4 bg-primary-700 text-white rounded p-2">{preferences.weight ? preferences.weight + " kg" : "?"}</div>
+            <input className="height outline-none focus:shadow-sm rounded-lg overflow-hidden appearance-none bg-gray-400 h-4 w-full" type="range" min={minWeight} max={maxWeight} step="1" defaultValue="70" onInput={e => setPreferences({...preferences, weight: e.target.value})}/>
+            <div className="flex justify-between">
+                <span>{minWeight} kg</span>
+                <span>{maxWeight * (5 / 8)} kg</span>
+                <span>{maxWeight * (6 / 8)} kg</span>
+                <span>{maxWeight * (7 / 8)} kg</span>
+                <span>{maxWeight} kg</span>
+            </div>
+
+            <div className="flex justify-between mt-8">
                 <button onClick={previousStep} type="button" className="w-1/3 py-4 bg-primary-700 text-white">
                     Wróć
                 </button>
